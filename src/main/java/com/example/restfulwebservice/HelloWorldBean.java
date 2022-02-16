c30f78a96753fe01dc122ce569a34dd74000a5e1
BYUNGJIN KIM <byuljin@BYUNGJINui-iMac.local>
2022-02-16 21:40:25 +0900

    Initial commit - Springboot 프로젝트 생성 - RestController 생성 - lombok 활용 Bean 객체 생성

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+//    public String getMessage() {
+//        return this.message;
+//    }
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+
+//    public HelloWorldBean(String hello_world) {
+//          this.message = message;
+//    }
+
+}
