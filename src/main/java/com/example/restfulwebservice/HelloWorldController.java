5a55c49b735da235c821519287df458d1058fa96
byuljin1 <bjkimg@gmail.com>
2022-02-16 22:35:16 +0900

    SpringBoot 프로젝트 생성 RestContoller 생성 lombok 활용 Bean 생성

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -10,7 +10,6 @@ public class HelloWorldController {
 //  GET
 //  /hello-world (endpoint)
 //  @RequestMapping(method= RequestMethod.GET, path="/hello-world")
-
     @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello World";
